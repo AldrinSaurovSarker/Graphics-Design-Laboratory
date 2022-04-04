bc1cc4df7fac5a4884c6ef302a4255071ad40e6e
Aldrin Saurov Sarker <A.saurov2016@gmail.com>
2022-04-04 12:34:16 +0600

    Update lab3 Ellipse.py

--- Ellipse/lab3 Ellipse.py
+++ Ellipse/lab3 Ellipse.py
@@ -2,38 +2,34 @@ import cv2
 import numpy as np
 
 
-def MidpointEllipse(image, X, Y, a, b):
-    x = 0
-    y = b
-    aa = a*a
-    bb = b*b
-    aa2 = aa*2
-    bb2 = bb*2
-    fx = 0
-    fy = aa2*b
-    p = bb-aa*b + 0.25*aa
+def MidpointEllipse(img, X, Y, a, b):
+    a, b = b, a
+    x, y = 0, b
+    aa, bb = a * a, b * b
+    aa2, bb2 = aa * 2, bb * 2
+    fx, fy = 0, aa2 * b
+    p = bb - (aa * b) + (0.25 * aa)
 
     while fx < fy:
-        image[X + x][Y + y] = 0
-        image[X + x][Y - y] = 0
-        image[X - x][Y + y] = 0
-        image[X - x][Y - y] = 0
-
+        img[X + x][Y + y] = 0
+        img[X + x][Y - y] = 0
+        img[X - x][Y + y] = 0
+        img[X - x][Y - y] = 0
         x += 1
-        fx = fx+bb2
+        fx = fx + bb2
 
         if p < 0:
-            p = p+fx + bb
+            p = p + fx + bb
         else:
             y -= 1
-            fy = fy-aa2
-            p = p+fx+bb-fy
-    image[X + x][Y + y] = 0
-    image[X + x][Y - y] = 0
-    image[X - x][Y + y] = 0
-    image[X - x][Y - y] = 0
+            fy = fy - aa2
+            p = p + fx + bb - fy
+    img[X + x][Y + y] = 0
+    img[X + x][Y - y] = 0
+    img[X - x][Y + y] = 0
+    img[X - x][Y - y] = 0
 
-    p = bb*(x+0.5)*(x+0.5) + aa*(y-1)*(y-1) - aa*bb
+    p = bb * (x + 0.5) * (x + 0.5) + aa * (y - 1) * (y - 1) - aa * bb
 
     while y > 0:
         y -= 1
@@ -44,20 +40,17 @@ def MidpointEllipse(image, X, Y, a, b):
             x += 1
             fx = fx + bb2
             p = p + fx - fy + aa
-        image[X + x][Y + y] = 0
-        image[X + x][Y - y] = 0
-        image[X - x][Y + y] = 0
-        image[X - x][Y - y] = 0
+        img[X + x][Y + y] = 0
+        img[X + x][Y - y] = 0
+        img[X - x][Y + y] = 0
+        img[X - x][Y - y] = 0
 
 
-img = np.ones([600, 600, 3])
+image = np.ones([600, 600, 3])
+Xc, Yc = map(int, input('Enter Center Point : ').split())
+A, B = map(int, input('Enter Major and Minor axes : ').split())
 
-print('Enter Center Point:')
-Xc, Yc = map(int, input().split())
-print('Enter major axis and minor axis:')
-A, B = map(int, input().split())
-MidpointEllipse(img, Xc, Yc, A, B)
-
-cv2.imshow('Output', img)
+MidpointEllipse(image, Xc, Yc, A, B)
+cv2.imshow('Output', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
